f67332ee557e4bf6655b888904a2bcf15345a960
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-06-03 13:40:35 +0200

    Window

--- Engine/Core/Window.cpp
+++ Engine/Core/Window.cpp
@@ -1,9 +1,9 @@
 #include "Window.h"
 
-Window::Window(std::string title) {
+Window::Window() {
     if (!glfwInit())
         return;
-    window = glfwCreateWindow(default_window_size[0], default_window_size[1], (title).c_str(), NULL, NULL);
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME, NULL, NULL);
     if (!window) glfwTerminate();
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
--- Engine/Core/Window.h
+++ Engine/Core/Window.h
@@ -2,7 +2,5 @@
 
 struct Window {
     GLFWwindow *window;
-    int default_window_size[2] = {1280, 720};
-    Window(std::string title);
-
+    Window();
 };
