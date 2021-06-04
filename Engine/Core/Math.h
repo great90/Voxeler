40ae9bedb62558e75f54d016da2fbad928deac2d
Kacper Kostka <kacper.kostka08@gmail.com>
2021-06-04 15:52:48 +0200

    Added Math.h

---
+++ Engine/Core/Math.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../Core.hpp"
+
+namespace Voxeler{
+    using vint = int;
+    using vfloat = float;
+    using vufloat = double;
+    using vlong = long;
+    using vshort = short;
+    using vchar = char;
+    using vstring = char*;
+    using vbool = bool;
+
+    using Vector3f = glm::fvec3;
+    using Vector3 = glm::vec3;
+
+	double Sin(double angle) { return glm::sin(angle); }
+	double Cos(double angle) { return glm::cos(angle); }
+	double Tan(double angle) { return glm::tan(angle); }
+}
