87738fc73ed18de11b67ee2694dab80efa505348
Kacper Kostka <kacper.kostka08@gmail.com>
2021-06-08 12:33:44 +0200

    General Update! v4 lol

---
+++ Engine/Core/ECS/Base/BaseComponent.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef ECS_ON
+
+#include "Types.h"
+
+namespace ECS {
+	struct BaseComponent {
+		BaseComponent() = default;
+		~BaseComponent() = default;
+
+		inline const EntityID GetID() const { return entityID; }
+
+	private:
+		EntityID entityID = -1;
+		friend class EntityManager;
+	};
+}
+
+#endif
---
+++ Engine/Core/ECS/ECS.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../Core.hpp"
+
+#ifdef ECS_ON
+    #include "Base/BaseComponent.h"
+    #include "Base/BaseSystem.h"
+    #include "Base/CompList.h"
+    #include "Base/ComponentFactory.h"
+    #include "Base/Entity.h"
+    #include "Base/EntityManager.h"
+    #include "Base/Types.h"
+#endif
